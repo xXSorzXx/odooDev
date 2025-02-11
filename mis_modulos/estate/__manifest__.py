014469c637f1fcf1cc57ec8eb3404b1e9490e654
xXSorzXx <jonathansor2000sm@gmail.com>
2025-02-11 16:41:22 -0600

    iniciando repositorio

---
+++ mis_modulos/estate/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'estate',
+    'version': '1.2',
+    'category': 'Sales/estate',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'description': "",
+    'depends': [
+        'base',        
+    ],
+    'data': [      
+    ],
+    'demo': [        
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ mis_modulos/estate/models/estate_property.py
@@ -0,0 +1,5 @@
+from odoo import models # type: ignore
+
+class estate_property(models.Model):
+    _name = "estate_property"
+    _description = "test jsor"
