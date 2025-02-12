d99cedf3f7d57157feb8ece61cf5816378705db8
xXSorzXx <jonathansor2000sm@gmail.com>
2025-02-11 18:05:12 -0600

    avances

--- mis_modulos/estate/models/estate_property.py
+++ mis_modulos/estate/models/estate_property.py
@@ -1,5 +1,23 @@
-from odoo import models # type: ignore
+from odoo import fields,models # type: ignore
 
 class estate_property(models.Model):
     _name = "estate_property"
     _description = "test jsor"
+
+    name = fields.Char(required=True)
+    description = fields.Text(string="label Ui")
+    postcode = fields.Char(help="help")
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='garden',
+        selection=[('north', 'North'),('south', 'South'), ('easth', 'Easth'), ('west', 'West')],
+        help="this is a test of selection"
+    )
