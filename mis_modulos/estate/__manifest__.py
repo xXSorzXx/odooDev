0f2ad6015cd58c43800bc66ef3a7212f8b63fa22
xXSorzXx <jonathansor2000sm@gmail.com>
2025-02-14 17:07:54 -0600

    Codigo con errores

--- mis_modulos/estate/__manifest__.py
+++ mis_modulos/estate/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'estate',
+    'name': 'estateSor',
     'version': '1.2',
     'category': 'Sales/estate',
     'sequence': 15,
@@ -12,7 +12,10 @@
         'base',        
     ],
     'data': [     
-        'security/ir.model.access.csv' 
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'        
     ],
     'demo': [        
     ],
